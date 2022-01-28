a152681f3e60fd20b9416a1fe34718aeb65ccba2
Adam Roesner <roesnera@kenyon.edu>
2022-01-28 18:16:57 -0500

    adds code from Renu into themeToggle.js, themeToggle.css. All works but css conflicts with other styling, so for now css link is commented out.

--- assets/JS/themeToggle.js
+++ assets/JS/themeToggle.js
@@ -1,6 +1,5 @@
-const toggleLightDark = () => {
-    const body = document.body;
-    const header = document.header;
-    body.classList.toggle('dark-mode');
-    header.classList.toggle('dark-mode');
-}
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+})
