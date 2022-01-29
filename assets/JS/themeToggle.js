2c436c35f89f542f0db534112d0f6c3266b59866
Renu <renunikhilp@gmail.com>
2022-01-29 14:36:19 -0800

    added JS toggleBtn

--- assets/JS/themeToggle.js
+++ assets/JS/themeToggle.js
@@ -1,6 +1,11 @@
-const toggleLightDark = () => {
-    const body = document.body;
-    const header = document.header;
-    body.classList.toggle('dark-mode');
-    header.classList.toggle('dark-mode');
-}
+// const toggleLightDark = () => {
+//     const body = document.body;
+//     const header = document.header;
+//     body.classList.toggle('dark-mode');
+//     header.classList.toggle('dark-mode');
+// }
+const checkbox = document.getElementById("checkbox");
+
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
